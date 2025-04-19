c0c0623c6a55843e2798f2a0d78445d472e93663
Henry <henry.arcangello@gmail.com>
2025-04-19 17:07:32 +0100

    auto

--- Autostart/arcangelo.sh
+++ Autostart/arcangelo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+chmod +x boot
 mv boot $HOME/
 # Update package list and install necessary packages
 echo "Updating package list and installing required packages..."
@@ -30,7 +30,8 @@ echo "Reloading zsh configuration..."
 # Installing Cool Retro Term -- Totally required, can't code without those retro screens. 
 echo "Installing Cool Retro Term for THAT vibe. . . . "
 wget https://github.com/Swordfish90/cool-retro-term/releases/download/continuous/Cool_Retro_Term-dac2b4f-x86_64.AppImage 
-mv Cool_Retro_Term-dac2b4f-x86_64.AppImage retro-terminal 
+mv Cool_Retro_Term-dac2b4f-x86_64.AppImage retro-terminal
+chmod +x retro-terminal 
 sudo mv retro-terminal /usr/bin/retro-terminal
 grep 'alias crterm="retro-terminal --fullscreen &"' ~/.zshrc || echo 'alias crterm="retro-terminal --fullscreen &"' >> ~/.zshrc; grep -qxF 'alias crterm="retro-terminal --fullscreen &"' ~/.bashrc || echo 'alias crterm="retro-terminal --fullscreen &"' >> ~/.bashrc
 echo " Done !"
