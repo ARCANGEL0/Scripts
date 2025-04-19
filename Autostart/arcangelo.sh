e2d5fe7643fb029f55d585f21ac9f18e589f1809
Henry <henry.arcangello@gmail.com>
2025-04-19 17:05:18 +0100

    auto

--- Autostart/arcangelo.sh
+++ Autostart/arcangelo.sh
@@ -31,7 +31,7 @@ echo "Reloading zsh configuration..."
 echo "Installing Cool Retro Term for THAT vibe. . . . "
 wget https://github.com/Swordfish90/cool-retro-term/releases/download/continuous/Cool_Retro_Term-dac2b4f-x86_64.AppImage 
 mv Cool_Retro_Term-dac2b4f-x86_64.AppImage retro-terminal 
-mv retro-terminal /usr/bin/retro-terminal
+sudo mv retro-terminal /usr/bin/retro-terminal
 grep 'alias crterm="retro-terminal --fullscreen &"' ~/.zshrc || echo 'alias crterm="retro-terminal --fullscreen &"' >> ~/.zshrc; grep -qxF 'alias crterm="retro-terminal --fullscreen &"' ~/.bashrc || echo 'alias crterm="retro-terminal --fullscreen &"' >> ~/.bashrc
 echo " Done !"
 clear
