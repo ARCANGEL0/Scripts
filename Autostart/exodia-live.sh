e0f34c79eb67509835c8e243bc438b4cf7eadd99
Henry <henry.arcangello@gmail.com>
2025-05-26 14:58:11 +0100

    set changes

--- Autostart/exodia-live.sh
+++ Autostart/exodia-live.sh
@@ -321,14 +321,11 @@ bspc wm -r
 
 # Configure Zsh prompt and theme
 echo "Configuring Zsh prompt..."
-cat <<"EOF" | sudo tee /home/liveuser/.zshrc
+sudo rm -f /home/liveuser/.zshrc
 
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]];
-    
-    then
-        
-        source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-
+cat <<"EOF" | sudo tee /home/liveuser/.zshrc > /dev/null
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
 # default editor #
@@ -349,7 +346,7 @@ plugins=(
   zsh-autosuggestions
   zsh-syntax-highlighting
   web-search
-  )
+)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -357,27 +354,16 @@ source $ZSH/oh-my-zsh.sh
 
 # On-demand rehash #
 zshcache_time="$(date +%s%N)"
-
 autoload -Uz add-zsh-hook
 
 rehash_precmd() {
-  if [[ -a /var/cache/zsh/pacman ]];
-      
-      then
-          
-          local paccache_time="$(date -r /var/cache/zsh/pacman +%s%N)"
-          
-          if (( zshcache_time < paccache_time ));
-              
-              then
-                  
-                  rehash
-                  zshcache_time="$paccache_time"
-
-          fi
-
+  if [[ -a /var/cache/zsh/pacman ]]; then
+    local paccache_time="$(date -r /var/cache/zsh/pacman +%s%N)"
+    if (( zshcache_time < paccache_time )); then
+      rehash
+      zshcache_time="$paccache_time"
+    fi
   fi
-
 }
 
 add-zsh-hook -Uz precmd rehash_precmd
@@ -436,77 +422,45 @@ alias MA="cd ~/.ncmpcpp/scripts/ && ./ncmpcpp-art"
 
 ## ------------ COLORS ------------ ##
 
-# Reset #
-RESET_COLOR='\033[0m' # Text Reset
+RESET_COLOR='\033[0m'
 
-# Regular Colors #
 Black='\033[0;30m'  Red='\033[0;31m'     Green='\033[0;32m'  Yellow='\033[0;33m'
 Blue='\033[0;34m'   Purple='\033[0;35m'  Cyan='\033[0;36m'   White='\033[0;37m'
 
-# Bold #
 BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m'
 BBlue='\033[1;34m'  BPurple='\033[1;35m' BCyan='\033[1;36m'  BWhite='\033[1;37m'
 
-# Underline #
 UBlack='\033[4;30m' URed='\033[4;31m'    UGreen='\033[4;32m' UYellow='\033[4;33m'
 UBlue='\033[4;34m'  UPurple='\033[4;35m' UCyan='\033[4;36m'  UWhite='\033[4;37m'
 
-# Background #
 On_Black='\033[40m' On_Red='\033[41m'    On_Green='\033[42m' On_Yellow='\033[43m'
 On_Blue='\033[44m'  On_Purple='\033[45m' On_Cyan='\033[46m'  On_White='\033[47m'
 
-# High Intensity #
-IBlack='\033[0;90m' IRed='\033[0;91m' IGreen='\033[0;92m' IYellow='\033[0;93m'      
-IBlue='\033[0;94m' IPurple='\033[0;95m' ICyan='\033[0;96m' IWhite='\033[0;97m'      
+IBlack='\033[0;90m' IRed='\033[0;91m' IGreen='\033[0;92m' IYellow='\033[0;93m'
+IBlue='\033[0;94m' IPurple='\033[0;95m' ICyan='\033[0;96m' IWhite='\033[0;97m'
 
-# Bold High Intensity #
 BIBlack='\033[1;90m' BIRed='\033[1;91m' BIGreen='\033[1;92m' BIYellow='\033[1;93m'
 BIBlue='\033[1;94m' BIPurple='\033[1;95m' BICyan='\033[1;96m' BIWhite='\033[1;97m'
 
-# High Intensity backgrounds #
 On_IBlack='\033[0;100m' On_IRed='\033[0;101m' On_IGreen='\033[0;102m' On_IYellow='\033[0;103m'
 On_IBlue='\033[0;104m' On_IPurple='\033[0;105m' On_ICyan='\033[0;106m' On_IWhite='\033[0;107m'
 
-# load on startup #
 echo -e "\033[1;31m
-		⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ▖ 
-		⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⡿⠟⠋⠀⠈⠙⠻⣷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀   ▌ ▛▌▛▌▛▌  
-		⠀⠀⠀⠀⠀⢀⣤⡀⠹⠛⠉⠀⣠⣴⣿⣷⣦⣀⠀⠉⠻⠏⢠⣄⡀⠀⠀⠀⠀⠀   ▙▖▙▌▌▌▙▌
-		⠀⠀⣀⣴⣾⠿⠋⠁⠀⠀⠀⠛⠉⢹⣿⣿⡇⠉⠓⠀⠀⠀⠈⠛⢿⣷⣤⣀⠀⠀         ▄▌
-		⣴⡿⠟⠋⠀⣀⣤⠶⠀⠀⠀⠀⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠶⣄⡀⠈⠙⠻⣷⣦   ▖ ▘   
-		⣿⡇⠀⣴⣾⣿⣤⡀⠀⠀⠀⠀⠀⠈⣿⣿⠀⠀⠀⠀⠀⠀⢀⣼⣿⣷⡆⠀⢸⣿   ▌ ▌▌▌█▌
-		⣿⡇⠀⣿⢿⣿⣿⣿⣶⣄⠀⠀⢀⣤⣿⣿⣤⡀⠀⢀⣠⣾⣿⣿⣿⣿⡇⠀⢸⣿   ▙▖▌▚▘▙▖
-		⣿⡇⠀⣿⠀⠈⠉⠛⠿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣷⣿⣿⠿⠛⠉⠀⠘⡇⠀⢸⣿
-		⠿⠇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠸⠟   ▗ ▌    ▄▖     ▘  
-		⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢰⣶   ▜▘▛▌█▌ ▙▖▛▛▌▛▌▌▛▘█▌
-		⣿⡇⠀⣿⠀⢀⣀⣤⣶⣿⣿⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣶⣤⣀⠀⢸⡇⠀⢸⣿   ▐▖▌▌▙▖ ▙▖▌▌▌▙▌▌▌ ▙▖
-		⣿⡇⠀⣿⣾⣿⣿⣿⠿⠋⠀⠀⠈⠛⣿⣿⠋⠁⠀⠈⠙⢿⣿⣿⣿⣿⡇⠀⢸⣿	            ▌    
-		⣿⡇⠀⠻⢿⣿⠛⠁⠀⠀⠀⠀⠀⢠⣿⣿⠀⠀⠀⠀⠀⠀⠈⢻⣿⡿⠇⠀⢸⣿
-		⠻⣿⣦⣄⡀⠉⠛⠶⠀⠀⠀⠀⠀⢸⣿⣿⡄⠀⠀⠀⠀⠀⠶⠋⠁⢀⣠⣶⡿⠟
-		⠀⠀⠉⠛⢿⣶⣤⡀⠀⠀⠀⣤⣀⣸⣿⣿⡇⣀⡤⠀⠀⠀⢀⣤⣾⡿⠛⠁⠀⠀
-		⠀⠀⠀⠀⠀⠈⠙⠁⣰⣦⣀⠀⠙⠻⣿⡿⠟⠉⠀⣀⣴⣆⠘⠋⠁⠀⠀⠀⠀⠀
-		⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣦⣄⠀⢀⣠⣴⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+... (your ASCII art here) ...
 \033[0m"
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh. #
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-# load custom profile #
-if [[ -f ~/.custom-profile ]];
-    
-    then
-        
-        . ~/.custom-profile
-
+if [[ -f ~/.custom-profile ]]; then
+  . ~/.custom-profile
 fi
 
-
-
-# change sudo prompt #
 export sudo_PROMPT="[] Enter sudo Password"
 EOF
 
+# Ensure ownership
+sudo chown liveuser:liveuser /home/liveuser/.zshrc
+nitrogen --set-zoom-fill --save --set-wallpaper "wallpaper.jpg"
 # Reload the shell configuration
 zsh
 
